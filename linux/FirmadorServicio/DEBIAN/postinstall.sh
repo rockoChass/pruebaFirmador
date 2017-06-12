af99e3dd98b9b7965801d0b0df3f525fefa2b92f
Angel Céspedes Quiroz <acespedes@adsib.gob.bo>
2017-06-12 17:44:42 -0400

    actualizando

--- linux/FirmadorServicio/DEBIAN/postinstall.sh
+++ linux/FirmadorServicio/DEBIAN/postinstall.sh
@@ -1,5 +1,3 @@
 echo "Iniciando servicio en segundo plano ....."
-sudo /etc/init.d/firmador-servicio stop
 sudo /etc/init.d/firmador-servicio start
-sudo /etc/init.d/firmador-servicio restart
 echo "FINALIZADO"
