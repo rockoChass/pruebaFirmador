8e9d6ff1df9fccdb0fb4a05048142943d71afe03
Angel Céspedes Quiroz <acespedes@adsib.gob.bo>
2017-06-13 16:32:05 -0400

    eliminando archivos sin uso

--- mac/FirmadorServicio/MAC/postinstall.sh
+++
@@ -1,5 +0,0 @@
-echo "Iniciando servicio en segundo plano ....."
-sudo firmador-servicio start
-echo "Configurando servicio para iniciar automaticamente ....."
-sudo update-rc.d firmador-servicio defaults
-echo "FINALIZADO"
