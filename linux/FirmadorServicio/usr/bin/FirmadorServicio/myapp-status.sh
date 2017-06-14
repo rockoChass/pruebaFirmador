6a77e5ab14b11ae833f179c80d9e123647ecfff8
Angel Céspedes Quiroz <acespedes@adsib.gob.bo>
2017-06-14 15:55:45 -0400

    status

--- linux/FirmadorServicio/usr/bin/FirmadorServicio/myapp-status.sh
+++ linux/FirmadorServicio/usr/bin/FirmadorServicio/myapp-status.sh
@@ -1,6 +1,6 @@
 pid=`ps aux | grep FirmadorServidor | awk '{print $2}'`
 if [ ! -f $pid ]; then
-  echo "el servicio esta iniciado ..."
-else
   echo "el servicio esta detenido ..."
+else
+  echo "el servicio esta iniciado ..."
 fi
