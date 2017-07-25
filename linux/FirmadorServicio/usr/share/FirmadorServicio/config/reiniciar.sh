2fdb66d02006b5aaaa4b1c8a85f0fa9a13254704
Angel Céspedes Quiroz <acespedes@adsib.gob.bo>
2017-07-25 11:13:51 -0400

    actualizando instaladores

--- linux/FirmadorServicio/usr/share/FirmadorServicio/config/reiniciar.sh
+++ linux/FirmadorServicio/usr/share/FirmadorServicio/config/reiniciar.sh
@@ -1,6 +1,6 @@
 echo "$SERVICE_NAME Deteniendo ..."
-sudo sh /usr/bin/FirmadorServicio/config/myapp-stop.sh
+sudo sh /usr/share/FirmadorServicio/config/myapp-stop.sh
 echo "$SERVICE_NAME Detenido ..."
 echo "Iniciando $SERVICE_NAME ..."
-sudo sh /usr/bin/FirmadorServicio/config/myapp-start.sh
+sudo sh /usr/share/FirmadorServicio/config/myapp-start.sh
 echo "$SERVICE_NAME Iniciado ..."
